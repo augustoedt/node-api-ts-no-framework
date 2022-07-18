d0714de1d73c8b00623671a93cc21a58c18414b6
Augusto Eduardo <augustoedt@gmail.com>
2022-07-18 19:11:02 -0300

    encapsulation server instance

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,26 @@
+import { rejects } from 'node:assert';
 import http from 'node:http'
+import { resolve } from 'node:path';
 import handler from './handler'
 
-const PORT = process.env.PORT || 3000
 
-const server = http.createServer(handler).listen(PORT, ()=>console.log(`server is running on port ${PORT}`))
+// const server = http.createServer(handler).listen(PORT, ()=>console.log(`server is running on port ${PORT}`))
 
+class ApiServer {
+    private server: any;
 
-export {
-    server
+    constructor(){
+        this.server = http.createServer(handler)
+    }
+
+    public Start = (port: any) =>{
+        return new Promise((resolve, rejects)=>{
+            this.server.listen(port,  ()=>console.log(`server is running on port ${PORT}`))
+        })
+    }
 }
+
+const PORT = process.env.PORT || 3000
+const server = new ApiServer().Start(PORT)
+
+export default ApiServer;
