45b144d285013538ce2281fe6fd4e1f67716f4eb
Augusto Eduardo <augustoedt@gmail.com>
2022-07-21 20:20:38 -0300

    fixing issues

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# node-api-ts-noframework
+# Node v18 CRUD in Typescript
+
+No Framework - NodeJs + Typescript Boilerplate API app
+
+[X] Full Typescript API
+[X] N-Layer Architecture API
+[X] Integration/Unit Test
+
--- test/integration/book.test.ts
+++ test/integration/book.test.ts
@@ -14,26 +14,26 @@ test('Book Integration Test Suite', async(t)=>{
     const server = instance.Start(TEST_PORT)
     const fetch = require('node-fetch')
 
-    // await t.test('create book', async (t)=>{
-    //     const book = {title: "test", description: "test description", author: "test author"};
+    await t.test('create book', async (t)=>{
+        const book = {title: "test", description: "test description", author: "test author"};
 
-    //     const request = await fetch(testServerAddress+"books", {
-    //         method: 'POST',
-    //         body: JSON.stringify(book)
-    //     })
+        const request = await fetch(testServerAddress+"books", {
+            method: 'POST',
+            body: JSON.stringify(book)
+        })
 
-    //     assert.deepStrictEqual(
-    //         request.headers.get('Content-Type'),
-    //         'application/json'
-    //     )
+        assert.deepStrictEqual(
+            request.headers.get('Content-Type'),
+            'application/json'
+        )
 
-    //     assert.strictEqual(request.status, 201)
+        assert.strictEqual(request.status, 201)
         
-    //     const result = await request.json()
+        const result = await request.json()
 
-    //     console.log(result)
+        console.log(result)
 
-    // })
+    })
 
     await t.test('update book', async (t)=>{
         const book = {id: "e0f55a18-7667-46ea-ae5d-e71487a751ce", title: "New Title"};
